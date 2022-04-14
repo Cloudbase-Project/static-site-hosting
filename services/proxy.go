0fb944244f1ca93f7a372c678f71abeaa8a90583
Navaneeth V <vnavaneeth15@gmail.com>
2022-04-14 22:19:52 +0530

    parse uuid from string

--- services/proxy.go
+++ services/proxy.go
@@ -19,11 +19,14 @@ func NewProxyService(db *gorm.DB, l *log.Logger) *ProxyService {
 	return &ProxyService{db: db, l: l}
 }
 
-func (ps *ProxyService) VerifySite(siteId interface{}) (*models.Site, error) {
+func (ps *ProxyService) VerifySite(siteId string) (*models.Site, error) {
 	var site models.Site
-	siteid, ok := siteId.(uuid.UUID)
-	fmt.Printf("ok: %v\n", ok)
-	if err := ps.db.Where(&models.Site{ID: siteid}).First(&site).Error; err != nil {
+	uniqueId, err := uuid.Parse(siteId)
+	if err != nil {
+		fmt.Printf("err.Error(): %v\n", err.Error())
+		return nil, err
+	}
+	if err := ps.db.Where(&models.Site{ID: uniqueId}).First(&site).Error; err != nil {
 		return nil, err
 	}
 	return &site, nil
